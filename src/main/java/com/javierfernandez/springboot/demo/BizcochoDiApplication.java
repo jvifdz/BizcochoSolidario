ecc5c1e73df4694de5a9e2a02bb55bf7f3168e6b
jvifdz <javiason@gmail.com>
2020-12-02 09:22:10 +0100

    Initial commit

---
+++ src/main/java/com/javierfernandez/springboot/demo/BizcochoDiApplication.java
@@ -0,0 +1,13 @@
+package com.javierfernandez.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizcochoDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BizcochoDiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javierfernandez/springboot/demo/BizcochoDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javierfernandez.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizcochoDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
