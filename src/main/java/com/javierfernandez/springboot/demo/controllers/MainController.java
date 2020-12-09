10e1b160f8218c536686d107bd91fabb8ad231e1
jvifdz <javiason@gmail.com>
2020-12-09 11:43:18 +0100

    Borrado de todo lo de Cliente

--- src/main/java/com/javierfernandez/springboot/demo/controllers/MainController.java
+++ src/main/java/com/javierfernandez/springboot/demo/controllers/MainController.java
@@ -2,9 +2,7 @@ package com.javierfernandez.springboot.demo.controllers;
 
 
 import com.javierfernandez.springboot.demo.models.dao.IBizcochoDao;
-import com.javierfernandez.springboot.demo.models.dao.ICLienteDao;
 import com.javierfernandez.springboot.demo.models.entity.Bizcocho;
-import com.javierfernandez.springboot.demo.models.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -20,39 +18,8 @@ import java.util.Map;
 public class MainController {
 
     @Autowired
-    @Qualifier("clienteDaoJPA")
-    private ICLienteDao clienteDao;
-
-    @Autowired
     @Qualifier("bizcochoDaoJPA")
     private IBizcochoDao bizcochoDao;
-
-
-
-    @RequestMapping(value = "/listar",method = RequestMethod.GET)
-    public String listar(Model model){
-        model.addAttribute("titulo","Listado de clientes");
-        model.addAttribute("clientes",clienteDao.findAll());
-        return "listar";
-    }
-
-    @RequestMapping(value = "/form")
-    public String crear(Map <String, Object> model){
-        Cliente cliente = new Cliente();
-        model.put("cliente", cliente);
-
-        model.put("titulo", "Formulario de Cliente");
-        return "form";
-    }
-
-    @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String guardar (Cliente cliente){
-        clienteDao.save(cliente);
-        return "redirect:listar";
-    }
-
-
-
 
     @RequestMapping(value = "/listarbizcocho",method = RequestMethod.GET)
     public String listarBizcocho(Model model){
